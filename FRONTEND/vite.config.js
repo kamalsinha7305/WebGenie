c140879848218edc0ecd8cf91fdc6804fbc2bcd8
kamal <kamalsinha7305@gmail.com>
2025-10-24 04:41:22 +0530

    updateforvercel

--- FRONTEND/vite.config.js
+++ FRONTEND/vite.config.js
@@ -1,7 +1,17 @@
+
+// vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    proxy: {
+      // This proxies requests from /api to your backend server
+      '/api': {
+        target: 'http://localhost:3001', // Your backend server URL
+        changeOrigin: true,
+      },
+    }
+  }
 })
